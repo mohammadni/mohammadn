7bd355dab5494b9dfffc2f7fb6da9dfaa516cb4f
mohammadni <mohammad8621447@gmail.com>
2017-01-22 02:15:01 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "get",
     "broadcast",
     "download_media",
-    "invite",
+    "invite",sudo_users = {324669120},--Sudo users
     "all",
     "leave_ban",
     "admin",
